12b61b7e0b04421edeeff18b4a84d25c2691889a
Luiz Atayde <atayde@gmail.com>
2020-11-03 11:04:43 -0300

    Correção versão Java, propriedades e senha.

--- src/main/java/com/atayde/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/atayde/api/MeuPrimeiroProjetoApplication.java
@@ -1,13 +1,41 @@
 package com.atayde.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.atayde.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
+	
+	@Value("${projeto.tipo}")
+	private String tipo;
+	
+	@Value("${projeto.valor}")
+	private int valor;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.err.println("### Valor propriedade (" + this.tipo + ") = " + this.valor);		
+			
+			
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.err.println("Senha encoded: " + senhaEncoded);	
+			
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.err.println("Senha encoded novamente: " + senhaEncoded);	
+
+			System.err.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));	
+			
+		};
 	}
 
 }
--- src/test/java/com/atayde/api/MeuPrimeiroProjetoApplicationTests.java
+++ src/test/java/com/atayde/api/MeuPrimeiroProjetoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.atayde.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MeuPrimeiroProjetoApplicationTests {
+	
+	@Value("${projeto.tipo}")
+	private String tipo;
+	
+	@Value("${projeto.valor}")
+	private int valor;
+
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void testPropriedades() {
+		assertEquals("test", tipo);
+		assertEquals(100, valor);
 	}
 
 }
